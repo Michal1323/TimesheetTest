0f95857396e387f729ae5a062c83ce6f0f381c79
amara1092 <amara1092@gmail.com>
2021-08-19 11:04:40 +0100

    User info is being drawn in

--- screens/login.js
+++ screens/login.js
@@ -7,6 +7,8 @@ import * as Linking from 'expo-linking';
 WebBrowser.maybeCompleteAuthSession();
 
 export default function login({ navigation }) { 
+
+  const [tok, setTok] = React.useState();
   // Endpoint
   const discovery = useAutoDiscovery('https://login.microsoftonline.com/c22e361c-58d9-4c39-a875-4b26582548fb/v2.0');
   // Request
@@ -49,7 +51,49 @@ export default function login({ navigation }) {
                     console.log('------- Before ----------------')
                     console.log(accessToken)
                     console.log('-----------------------')
-                } catch (e) {
+                    setTok(accessToken);
+                    
+                      /* make a GET request using fetch and querying with the token */
+                      let graphResponse = null;
+                      await fetch('https://graph.microsoft.com/v1.0/me?$select=surname', {
+                        method: 'GET',
+                        headers: {
+                          'Authorization': 'Bearer ' + accessToken,
+                        }
+                      })
+                      .then((response) => response.json())
+                      .then((response) => {
+                        graphResponse = response;
+
+                      })
+                      .catch((error) => {
+                        graphResponse = error;
+                      });
+
+                      /* 
+    Spread the results of the graph and add a type property with a value of success to indicate
+    that the AzureAD info grabbing was a success
+  */ 
+  const finalResponse = {
+    ...graphResponse,
+    type: "success",
+   
+  }
+  console.log(finalResponse);
+  var json = JSON.stringify(finalResponse);
+  console.log("------------------------------------------")
+  console.log(json)
+  var obj = JSON.parse(json);
+  console.log("------------------------------------------")
+console.log(obj.surname)
+
+  return finalResponse;
+ //end callMsGraph()
+
+
+               
+                  } 
+                catch (e) {
                     console.log(e)
                     console.log(e.status)
                     console.log(e.message)
@@ -57,7 +101,10 @@ export default function login({ navigation }) {
             })()
         }
     }
+   
 }, [response]);
+
+
 
 
   return (
